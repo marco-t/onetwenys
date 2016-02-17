2fa83bb0f2e863c2b9228cd335b68aa33e7efb1a
marco-t <tobine22@hotmail.com>
2016-02-17 10:25:20 -0330

    Change dealer after round

--- onetwenys.rb
+++ onetwenys.rb
@@ -33,13 +33,18 @@ class Card
 end
 
 class Team
+	attr_accessor :score, :mate1, :mate2
+	def initialize(teammate1, teammate2)
+		@mate1 = teammate1
+		@mate2 = teammate2
+		@score = 0
+	end
 end
 
 class Player
-	attr_accessor :hand, :score, :dealer
+	attr_accessor :hand, :dealer
 	def initialize
 		@hand = []
-		@score = 0
 		@dealer = false
 	end
 
@@ -48,101 +53,114 @@ class Player
 		other_player.dealer = true
 	end
 
-	def deal(deck, player1, player2, kitty)
-		player1.hand.clear
-		player2.hand.clear
-		player1.hand << deck.cards.pop(5)
-		player2.hand << deck.cards.pop(5)
+	def deal(deck, players, kitty)
+		# clear old hands if any, deal 5 cards to each player
+		players.each do |player|
+			player.hand.clear
+			player.hand << deck.cards.pop(5)
+		end
+
+		# add three cards to empty kitty
 		kitty << deck.cards.pop(3)
 	end
 end
 
 class Round
-	def initialize(player1, player2)
-		@player1 = player1
-		@player2 = player2
+	def initialize(players, teams)
+		@players = players
+		@teams = teams
+		
 		@kitty = []
 		@deck = Deck.new
-		# refactor this terrible code
-		if player1.dealer
-			@dealer = player1
-			@next_to_dealer = player2
-		else
-			@dealer = player2
-			@next_to_dealer = player1
+
+		# who is the dealer this round?
+		players.each do |player|
+			@dealer = player if player.dealer == true
 		end
 	end
 
-	def bid
+	def bet
 	end
 
 	def play_round
-		@dealer.deal(@deck, @player1, @player2, @kitty)
+		@dealer.deal(@deck, @players, @kitty)
 
 		roundOver = false
 		tricks_played = 0
 
 		until roundOver do
-			trick = Trick.new(@player1, @player2)
+			trick = Trick.new(@players, @teams)
 			trick.play_trick
 			tricks_played += 1
 			if tricks_played == 5
 				roundOver = true
 			end
 		end
-		@dealer.pass_deck(@next_to_dealer)
+
+		# index of dealer
+		i = @players.rindex do |player|
+			player.dealer == true
+		end
+
+		@players[i].dealer = false
+		# if last player was dealer make first player dealer
+		@players[i+1].nil? ? @players[0].dealer = true : @players[i+1].dealer = true
 	end
 end
 
 class Trick
-	def initialize(player1, player2)
-		@player1 = player1
-		@player2 = player2
+	def initialize(players, teams)
+		@players = players
+		@teams = teams
 	end
 
 	def play_trick
-		win_trick(@player1)
+
+		win_trick(@teams[0])
 	end
 
-	def win_trick(player)
-		player.score += 5
+	def win_trick(team)
+		team.score += 5
 		# player.score += 10 if 5 of trump in trick
 	end
 
 end
 
 class Game
-	def initialize
+	# number of players must be even
+	def initialize(number_of_players)
 		@gameOver = false
-		@player1 = Player.new
-		@player2 = Player.new
-		if rand(2) == 0
-			@player1.dealer = true
-		else
-			@player2.dealer = true
+		@players = []
+		number_of_players.times do |i|
+			@players << Player.new
 		end
+
+		# assign teams
+		number_of_teams = number_of_players / 2
+		@teams = []
+		number_of_teams.times do |i|
+			@teams << Team.new(@players[i], @players[i+2])
+		end
+
+		# draw for dealer
+		rand = rand(number_of_players)
+		@players[rand].dealer = true
 	end
 
 	def play_game
 		# player wins when their score reaches 120 or more.
 		# player can only win if they placed the last bet
-		until (@player1.score >= 120 || @player2.score >= 120) do
-			round = Round.new(@player1, @player2)
+		#until @gameOver do
+		1.times do
+			round = Round.new(@players, @teams)
 			round.play_round
-		end
+			@teams.each do |team|
+				@gameOver = true if team.score >= 120
+			end
+		end			
 	end
 end
 
-game = Game.new
+number_of_players = 4
+game = Game.new(number_of_players)
 game.play_game
-
-# player1 = Player.new
-# player2 = Player.new
-# deck = Deck.new
-# kitty = []
-# deck.deal(player1, player2, kitty)
-# puts player1.hand
-# puts
-# puts player2.hand
-# puts
-# puts kitty
