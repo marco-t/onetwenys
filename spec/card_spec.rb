d983647e790288c95d55cdef2270aefac9d17a88
marco-t <tobine22@hotmail.com>
2016-02-27 22:37:35 -0330

    Added new spec tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,14 +3,34 @@ require_relative '../onetwenys'
 describe Card do
   before(:each) do
     @card = Card.new
+    @trump = :clubs
+    @card.update(name: :'10c', rank: '10', suit: @trump, color: :black)
   end
 
-  # to be implemented
-  # context 'trump' do
-  #   it 'should have value greater than 35' do
-  #     @card[:trump] = true
-  #     @card.set_value
-  #     expect(@card[:value]).to be > 35
-  #   end
-  # end
+  it 'Ace of Hearts should be trump' do
+    @card.update(name: :Ah, rank: 'A', suit: :hearts, color: :red)
+    @card.set_value(@trump)
+    expect(@card[:trump]).to be true
+  end
+
+  it 'should have value greater than 35' do
+    @card.set_value(@trump)
+    expect(@card[:value]).to be > 35
+  end
+
+  it 'first card laid has value between 0 and 35' do
+    first_card = Card.new
+    first_card.update(name: :'10h', rank: '10', suit: :hearts, color: :red)
+    first_card.set_value(@trump)
+    expect(first_card[:value]).to be > 0
+    expect(first_card[:value]).to be < 35
+  end
+
+  it 'second card has no value if off-suit and not trump' do
+    first_card, second_card = Card.new, Card.new
+    first_card.update(name: :'10h', rank: '10', suit: :hearts, color: :red)
+    second_card.update(name: :'10d', rank: '10', suit: :diamonds, color: :red)
+    second_card.set_value(@trump, first_card)
+    expect(second_card[:value]).to be 0
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -8,4 +8,20 @@ describe Deck do
   it 'should have 52 cards' do
     expect(@deck.cards.length).to eq 52
   end
+
+  it 'should be shuffled' do
+    suits = [:clubs, :spades, :hearts, :diamonds]
+    numbers = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+    cards = []
+    suits.each do |suit|
+      numbers.each do |number|
+        abrv = (number.to_s + suit.to_s.chr).to_sym
+        card = Card.new
+        card.update(name: abrv)
+        cards << card
+      end
+    end
+
+    expect(@deck.cards).not_to eql cards
+  end
 end
